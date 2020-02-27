917c7253fe3797a9f971e736e93b6722add2f54a
mpyw <ryosuke_i_628@yahoo.co.jp>
2020-02-28 07:19:40 +0900

    Feat+Refactor: Introduce a wrapper for scalar values and refactor with it (#4)
    * Feat: Introduce a wrapper for scalar values
    
    * Refactor: Refactor using Value wrapper
    
    * Test: Update tests allowing minor breaking changes

--- src/SystemVariableGrammar.php
+++ src/SystemVariableGrammar.php
@@ -2,8 +2,6 @@
 
 namespace Mpyw\LaravelMySqlSystemVariableManager;
 
-use PDO;
-
 class SystemVariableGrammar
 {
     /**
@@ -23,40 +21,9 @@ class SystemVariableGrammar
     {
         $expressions = [];
         foreach ($values as $name => $value) {
-            $expressions[] = static::escapeIdentifier($name) . '=' . static::placeholderFor($value);
+            $expressions[] = static::escapeIdentifier($name) . '=' . Value::wrap($value)->getPlaceholder();
         }
         return $expressions;
-    }
-
-    /**
-     * @param  mixed $value
-     * @return int
-     */
-    public static function paramTypeFor($value): int
-    {
-        switch (gettype($value)) {
-            case 'integer':
-                return PDO::PARAM_INT;
-            case 'boolean':
-                return PDO::PARAM_BOOL;
-            case 'NULL':
-            default:
-                return PDO::PARAM_STR;
-        }
-    }
-
-    /**
-     * @param  mixed  $value
-     * @return string
-     */
-    public static function placeholderFor($value): string
-    {
-        switch (gettype($value)) {
-            case 'double':
-                return 'cast(? as decimal(65, 30))';
-            default:
-                return '?';
-        }
     }
 
     /**
